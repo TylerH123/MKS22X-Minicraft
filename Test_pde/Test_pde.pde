0a304c42ca20c000ed534509eab369383c71878e
Moody Rahman <mrahman01@stuy.edu>
2019-05-20 21:31:05 -0400

    leaning to indicate current direction travelling

--- Test_pde/Test_pde.pde
+++ Test_pde/Test_pde.pde
@@ -1,5 +1,6 @@
 boolean keyz[] = new boolean [4];
 float dx, dy;
+float leanx, leany;
 void setup() {
   size(1000, 1200);
   noStroke();
@@ -8,28 +9,34 @@ void setup() {
 
 void draw() {
   background(80);
+  leanx = 0;
+  leany = 0;
   if (keyz[0]){
     dx+= 5;
+    leanx = -5;
   }
   if (keyz[1]){
     dy-= 5;
+    leany = 5;
   }
   if (keyz[2]){
     dx-= 5;
+    leanx = 5;
   }
   if (keyz[3]){
     dy+= 5;
+    leany = -5;
   }
+  fill(5);
 
   text("" + dx + ", " + dy, 10, 10);
-  fill(88, 185, 118);
-  text("PLAYER BOI", 400, 500);
 
   fill(204, 153, 21);
   rect(dx+100, dy+100, 30, 50);
 
-  fill(5);
-  rect(400, 500, 50, 50);
+  fill(88, 185, 118);
+  text("PLAYER BOI", 400, 500);
+  rect(400 + leanx, 500 + leany, 50, 50);
 }
 
 void keyPressed() {
--- Test_pde/build/source/Test_pde.java
+++ Test_pde/build/source/Test_pde.java
@@ -16,6 +16,7 @@ public class Test_pde extends PApplet {
 
 boolean keyz[] = new boolean [4];
 float dx, dy;
+float leanx, leany;
 public void setup() {
   
   noStroke();
@@ -24,28 +25,34 @@ public void setup() {
 
 public void draw() {
   background(80);
+  leanx = 0;
+  leany = 0;
   if (keyz[0]){
     dx+= 5;
+    leanx = -5;
   }
   if (keyz[1]){
     dy-= 5;
+    leany = 5;
   }
   if (keyz[2]){
     dx-= 5;
+    leanx = 5;
   }
   if (keyz[3]){
     dy+= 5;
+    leany = -5;
   }
+  fill(5);
 
   text("" + dx + ", " + dy, 10, 10);
-  fill(88, 185, 118);
-  text("PLAYER BOI", 400, 500);
 
   fill(204, 153, 21);
   rect(dx+100, dy+100, 30, 50);
 
-  fill(5);
-  rect(400, 500, 50, 50);
+  fill(88, 185, 118);
+  text("PLAYER BOI", 400, 500);
+  rect(400 + leanx, 500 + leany, 50, 50);
 }
 
 public void keyPressed() {
